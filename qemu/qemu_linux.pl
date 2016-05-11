e327642bbbf09b77da051172118b58560e4822b8
John Phillips <john.phillips5@hpe.com>
2016-05-11 22:24:11 +0000

    Make the qemu_linux script better
    I was wasting code by not redirecting file descriptors from the start.

--- qemu/qemu_linux.pl
+++ qemu/qemu_linux.pl
@@ -1,95 +1,82 @@
 #!/usr/bin/perl
 use POSIX ":sys_wait_h";
+use IPC::Cmd qw(can_run run_forked);
 use strict;
 use warnings;
 use v5.20;
+use Carp;
 use IPC::Open3;
 use IO::Select;
 
-my $vnc_port = 5;
-my $memory_gb =  4;
-my $use_vnc = "no";
-my $background = 0;
-my $imgloc = undef;
-my $isoloc = undef;
-my $vhostuser_port = "vhost-user-1";     
+my %pids=();
+my $VNC_CMD = "vncviewer";
+my $KVM_CMD = "kvm";
 
-while ((my $var = shift @ARGV)) {
-    if ($var eq "--mem-gb") {
-        $memory_gb = shift @ARGV;
-    } elsif($var eq "--use-vnc") {
-        $use_vnc = shift @ARGV; 
-    } elsif($var eq "--vnc-port") {
-        $vnc_port = shift @ARGV;
-    } elsif($var eq "--background") {
-        $background = 1;
-    } elsif($var eq "--imgloc") {
-        $imgloc = shift @ARGV;
-    } elsif($var eq "--isoloc") {
-        $isoloc = shift @ARGV;
-    } elsif($var eq "--vhostuser-sock") {
-        $vhostuser_port = shift @ARGV;
-    } else {
-        usage_and_exit();
+sub child_exited {
+    my $pid=0;
+    my @deceased = ();
+    do {
+        $pid = waitpid(-1, WNOHANG);
+        push @deceased, $pid;
+    } while $pid > 0; 
+
+    foreach $pid (@deceased) {
+        next if $pid <= 0;
+        say "Child: $pid cmdline: $pids{$pid} exited.";
+        delete $pids{$pid};
     }
 }
 
-die "I need an img/iso location" if not (defined $imgloc or defined $isoloc);
-my $mem_mb = $memory_gb * 1024;
-my $mem_gb = $memory_gb . "G";
-my $cmdline = "sudo kvm "
-. "-boot order=cd "
-. "-cpu host "
-. "-vnc :$vnc_port "
-. "-m " . $memory_gb * 1024 ." "
-. "-name  'hlinux qemu' ";
-$cmdline .= "-cdrom $isoloc " if defined $isoloc;
-$cmdline .= "-drive file=$imgloc " if defined $imgloc;
-$cmdline .=
-"-chardev socket,id=char1,path=/var/run/openvswitch/$vhostuser_port "
-. "-netdev type=vhost-user,id=mynet1,chardev=char1,vhostforce "
-. "-device virtio-net-pci,mac=00:00:00:00:00:01,netdev=mynet1 "
-. "-object memory-backend-file,id=mem,size=" . $mem_gb . ",mem-path=/dev/hugepages,share=on "
-. "-numa node,memdev=mem -mem-prealloc "
-. "-netdev tap,id=mynet2 "
-. "-device virtio-net-pci,netdev=mynet2 ";
+$SIG{CHLD} = \&child_exited;
 
-#my $cmdline =<<"_EOF_";
-#sudo kvm
-#-boot order=cd
-#-cpu host
-#-vnc :$vnc_port
-#-m $mem_mb
-#-name 'hlinux qemu'
-#-cdrom hlinux-iso.iso
-#-drive file=hlinux.img
-#-chardev socket,id=char1,path=/var/run/openvswitch/vhost-user-1 
-#-netdev type=vhost-user,id=mynet1,chardev=char1,vhostforce 
-#-device virtio-net-pci,mac=00:00:00:00:00:01,netdev=mynet1 
-#-object memory-backend-file,id=mem,size=$mem_gb,mem-path=/dev/hugepages,share=on 
-#-numa node,memdev=mem -mem-prealloc 
-#-netdev tap,id=mynet2 
-#-device virtio-net-pci,netdev=mynet2
-#_EOF_
+my %args = (
+    vnc_port => 5,
+    memory_gb => 4,
+    use_vnc => 0,
+    background => 0,
+    imgloc => undef,
+    isoloc => undef,
+    vhostuser_sock => undef,
+    use_hugepage_backend => 0);
 
-my $vnc =<<"_EOF_";
-vncviewer 'localhost::590$vnc_port'
-_EOF_
-$cmdline =~ tr/\n/ /;
-$vnc =~ tr/\n/ /;
+while((my $var = shift @ARGV)) {
+    if(not $var =~ m/^\-\-.*/) {
+        croak "Bad argument: $var";
+    }
+    $var = substr($var, 2);
+    $var =~ tr/\-/_/;
+    if ($var eq "help") {
+        usage_and_exit();
+    }
+    if (not exists($args{$var})) {
+        croak "Bad key: $var";
+    } else {
+        $args{$var} = shift @ARGV;
+        if ($args{$var} =~ /[Nn][Oo].*/) {
+            $args{$var} = 0;
+        } elsif($args{$var} eq "yes") {
+            $args{$var} = 1;
+        }
+    }
+}
 
-my %pids=();
-my ($vnc_pid, $vnc_stdout, $vnc_stderr) = (undef, undef, undef);
-my ($q_pid, $q_stdout, $q_stderr) = process_exec($cmdline);
 
-@{$pids{$q_pid}} = ($q_stdout, $q_stderr);
-if ($use_vnc =~ /[Yy](es)?/) {
+my $vnc = create_vnc_command(%args);
+croak "Undefined vnc command" if not defined $vnc and $args{use_vnc} eq "yes";
+my $qemu = create_qemu_command(%args);
+croak "Undefined qemu command" if not defined $qemu;
+
+
+my $qemu_pid = process_exec($qemu) or croak "Can't start qemu: $qemu";
+my $vnc_pid = undef;
+
+if ($args{use_vnc}) {
     say "Wating for VM to get going to start VNC...";
     sleep 2;
-    ($vnc_pid, $vnc_stdout, $vnc_stderr) = process_exec($vnc);
-    @{$pids{$vnc_pid}} = ($vnc_stdout, $vnc_stderr);
+    process_exec($vnc) or croak "Can't start VNC";
 }
-if ($background) {
+
+if ($args{background}) {
     my $pid = fork;
     if ($pid != 0) {
         say "Backgrounding.";
@@ -100,46 +87,86 @@ if ($background) {
     open STDERR, ">&STDOUT";
 }
 
-my $s = IO::Select->new();
-my @ready=();
-$s->add($q_stdout, $q_stderr, $vnc_stdout, $vnc_stderr);
+
+
+#my $s = IO::Select->new();
+#my @ready=();
+#$s->add($q_stdout, $q_stderr, $vnc_stdout, $vnc_stderr);
+
 for (; ; ) {
-    my $pid = waitpid(-1, WNOHANG);
-
-    @ready = $s->can_read(0);
-    for my $fh (@ready) {
-        if ($background) {
-            # Throw away output if backgrounded.
-            <$fh>;
-        } else {
-            print <$fh>;
-        }
-    }
-
-    if ($pid > 0) {
-        say "**A child ($pid) exited code $?**";
-        if (scalar(@{$pids{$pid}}) > 0)  {
-            $s->remove(@{$pids{$pid}});
-            if (not scalar $s->handles) {
-                say "All children exited";
-                exit;
-            }
-        } else {
-            say "But I'm not polling that!";
-        }
+    sleep 1;
+    if (scalar keys %pids == 0) {
+        say "All children exited\n";
+        exit 0;
     }
 }
 
 sub process_exec {
     my $cmdline = shift;
     my ($write, $read, $err);
-    my $pid = open3($write, $read, $err, $cmdline) or do {
+    my $pid = open3(\*STDIN, ">&STDOUT", ">&STDERR", $cmdline) or do {
         die "Couldn't fork for child\n";
     };
 
     say "Forked child process $cmdline ($pid)";
-    close $write;
-    return ($pid, $read, $err);
+    $pids{$pid} = $cmdline;
+    return 1;
+}
+
+sub create_qemu_command {
+    my %args = (
+        vnc_port=>undef,
+        imgloc=>undef,
+        memory_gb=>undef,
+        use_hugepage_backend=>undef,
+        vhost_user_port=>undef,
+        @_,
+    );
+
+    die "I need an img/iso location" if not (defined $args{imgloc} or defined $args{isoloc});
+
+    my $mem_mb = $args{memory_gb} * 1024;
+    my $mem_gb = $args{memory_gb} . "G";
+
+    croak "Can't run $KVM_CMD" if not can_run($KVM_CMD);
+    my $cmdline = "sudo $KVM_CMD "
+    . "-boot order=cd "
+    . "-cpu host "
+    . "-vnc :$args{vnc_port} "
+    . "-m " . $mem_mb ." "
+    . "-name  'hlinux qemu' ";
+    $cmdline .= "-cdrom $args{isoloc} " if defined $args{isoloc};
+    $cmdline .= "-drive file=$args{imgloc} " if defined $args{imgloc};
+
+    $cmdline .=
+    "-chardev socket,id=char1,path=/var/run/openvswitch/$args{vhostuser_port} "
+    . "-netdev type=vhost-user,id=mynet1,chardev=char1,vhostforce "
+    . "-device virtio-net-pci,mac=00:00:00:00:00:01,netdev=mynet1 " if $args{vhostuser_port};
+
+    $cmdline .=
+    "-object memory-backend-file,id=mem,size=" . $mem_gb . ",mem-path=/dev/hugepages,share=on " 
+    . "-numa node,memdev=mem -mem-prealloc " if $args{use_hugepage_backend};
+
+    $cmdline .=
+    "-netdev tap,id=mynet2 "
+    . "-device virtio-net-pci,netdev=mynet2 ";
+
+    $cmdline =~ tr/\n/ /;
+    return $cmdline;
+}
+
+sub create_vnc_command {
+    my %args = (
+        vnc_port=>undef,
+        @_,
+    );
+    croak "Undefined vnc_port" if not defined $args{vnc_port};
+    my $vnc = undef;
+    if (can_run($VNC_CMD)) {
+        $vnc = "$VNC_CMD localhost::590$args{vnc_port}";
+        $vnc =~ tr/\n/ /;
+    }
+    return $vnc;
 }
 
 sub usage_and_exit {
